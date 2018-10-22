add8b3b7e7aa4616af63e62cb42749285f32f6fe
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-10-22 10:00:05 +0900

    first commit

---
+++ Scripts/TC1/Script1540167526500.groovy
@@ -0,0 +1,9 @@
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.google.com/search?q=wikipedia&ie=utf-8&oe=utf-8')
+
+WebUI.delay(5)
+
+WebUI.closeBrowser('')
